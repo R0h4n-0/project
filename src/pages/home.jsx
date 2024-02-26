54a98c03e14c30ebb4b929d71ec522c88fc3c7aa
Rohan <walkerrandy930@gmail.com>
2024-02-27 01:17:40 +0545

    1 am push

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,18 @@
+import React from 'react';
+import bgimg from '../images/bgimg.jpg';
+
 export default function Home() {
-    return <h2>Hello</h2>
-  
-  }
+
+  return (
+    <div
+      className={`bg-cover bg-no-repeat h-screen`}
+      style={{
+        backgroundImage: `url(${bgimg})`,
+      }}
+    >
+      <div className="flex items-center justify-center h-full">
+        <h2 className="text-2xl font-bold">Hello</h2>
+      </div>
+    </div>
+  );
+}
